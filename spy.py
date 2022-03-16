1851bb9bb8324fce472d3857dac5b3b9532326ba
deathsmilestome <vanekt35@mail.ru>
2022-03-16 23:48:04 +0300

    +add locs command

--- spy.py
+++ spy.py
@@ -1,30 +1,37 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random
 import os
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
-
+temp = ""
 members = list()
-locs = ('Бонч', 'Аквапарк', 'Цирк', 'Подводная лодка', 'Космическая станция')
+locs = ['Бонч', 'Аквапарк', 'Цирк', 'Подводная лодка', 'Космическая станция']
 
-#goCheck = InlineKeyboardButton(text='GO!', callback_data='go')
-#go = InlineKeyboardButton(resize_keyboard=True)
-#go.row(goCheck)
 async def on_startup(_):
 	print('Started')
 
 @dp.message_handler(commands=['start'])
 async def command_start(message : types.Message):
 	await message.answer("Есть список локаций, все из вас, кроме одного, получают одну из этих локаций, игроки, которые получили локацию должны задавать друг другу вопросы про эту локацию и вычислять того, кто локацию не получил. Человек, который не получил локацию - шпион, ты должен интуитивно отвечать на вопросы, пытайся не спалиться, твоя задача угадать, что это за локация прежде чем тебя вычислят. GLHF")
-
+	await message.answer(locs)
 @dp.message_handler(commands=['loc'])
 async def command_start(message : types.Message):
-	await message.answer("Бонч|Аквапарк|Цирк|Подводная лодка|Космическая станция")
+	await message.answer(locs)
+
+@dp.message_handler(commands=['newloc'])
+async def command_start(message : types.Message):
+	await message.answer("Для добавления новой локации отправьте сообщение типа +[название локации]" )
+
+@dp.message_handler(text_contains='+[')
+async def about_us(message :types.Message):
+	f = filter(str.isalpha, message.text)
+	temp = "".join(f)
+	locs.append(temp)
+	if temp in locs:
+		await message.answer("Добавлено!")
 
 @dp.message_handler(commands=['play'])
 async def command_start(message : types.Message):
@@ -47,8 +54,5 @@ async def about_us(message :types.Message):
 		if new not in members:
 			members.append(new)
 			await message.reply("nice")
-
-
-
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
